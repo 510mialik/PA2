f19f88b4409bf5cef32fe55764294445b7f74f05
unknown <okhomyak+git@gmail.com>
2015-05-06 23:57:07 -0700

    _

--- hash.cpp
+++ hash.cpp
@@ -1,6 +1,54 @@
 // for hash function
+#include <iostream>
+#include <string>
+#include <fstream>
+using namespace std;
 
-int hash (string s)
+int hash (string s, int size);
+void normalize (string &s);
+
+const int MAX_SIZE = 10000;
+
+int main ()
 {
-  
+	ifstream in;
+	in.open ("words.txt");
+	if (!in)
+	{
+		cout << "ERROR OPENING FILE!" << endl;
+		return -1;
+	}
+	string s;
+	int words = 0;
+	while (!in.eof())
+	{
+		in >> s;
+		normalize (s);
+	}
+	return 0;
+}
+
+int hash (string s, int size)
+{
+	int h = 0;
+
+	return h;
+}
+
+
+void normalize (string &s)
+{
+	string t = "";
+	for (int i = 0; i < s.length(); i++)
+	{
+		if (s[i] <= 'Z' && s[i] >= 'A')
+		{
+			s[i] += ('a' - 'A');
+		}
+		if (s[i] <= 'z' && s[i] >= 'a')
+		{
+			t+= s[i];
+		}
+	}
+	s = t;
 }
